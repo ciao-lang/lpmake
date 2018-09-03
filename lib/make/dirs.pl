631a116cfc733911dddd8c75ee38eb43a04c4bbf
Jose F. Morales <jfmcjf@gmail.com>
2018-09-03 11:58:38 +0200

    Moved data,concurrent operators (datafacts.pl) and dynamic (dynamic.pl)
    Note that this do not prevent the compiler to accept data, concurrent,
    or dynamic predicates. They can still be declared with e.g,
    ":- data(foo/1)".
    
    Actually disabling predicate "kinds" require adding compiler flags
    and/or declarations (future work).
    
    Src-commit: 1d00be0bd468840ada51143fd0139110f38ee0d6

--- lib/make/dirs.pl
+++ lib/make/dirs.pl
@@ -1,5 +1,5 @@
 :- module(dirs, []).
 :- use_package([]).
 :- multifile library_directory/1.
-:- dynamic library_directory/1.
-library_directory('.').
+:- dynamic(library_directory/1). % (just declaration, dynamic not needed in this module)
+library_directory('.'). % TODO: strange
